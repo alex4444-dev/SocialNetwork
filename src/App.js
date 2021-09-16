3d69d3f1f603344b1849cf22662f17a1a83b29be
alex4444-dev <grigoralex@mail.ru>
2021-09-16 23:33:31 +0300

    the same mistake

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import './App.css';
 import Header from './Components/Header/Header';
 import Navbar from './Components/Navbar/Navbar';
 import {BrowserRouter, Route} from "react-router-dom";
-import Dialogs from "./Components/Dialogs/Dialogs";
 import Profile from "./Components/Profile/Profile";
 import DialogsContainer from "./Components/Dialogs/DialogsContainer";
 import Users from "./Components/Users/Users";
+
 
 
 const App = (props) => {
